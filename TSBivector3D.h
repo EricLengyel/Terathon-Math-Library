56dba21f02fb756a31f9158a9dee0ce7d78d3993
Eric Lengyel <lengyel@terathon.com>
2020-12-07 21:06:48 -0800

    Added documentation

--- TSBivector3D.h
+++ TSBivector3D.h
@@ -39,6 +39,122 @@ namespace Terathon
 	};
 
 
+	//# \class	Bivector3D		Encapsulates a 3D bivector.
+	//
+	//# The $Bivector3D$ class encapsulates a 3D bivector.
+	//
+	//# \def	class Bivector3D
+	//
+	//# \ctor	Bivector3D();
+	//# \ctor	Bivector3D(float a, float b, float c);
+	//# \ctor	Bivector4D(const Point2D& p, const Point2D& q);
+	//# \ctor	Bivector4D(const Point2D& p, const Vector2D& v);
+	//
+	//# \param	a		The value of the <b>e</b><sub>23</sub> coordinate.
+	//# \param	b		The value of the <b>e</b><sub>31</sub> coordinate.
+	//# \param	c		The value of the <b>e</b><sub>12</sub> coordinate.
+	//# \param	p,q		Two 2D points that lie on the line.
+	//# \param	v		A 2D direction vector.
+	//
+	//# \desc
+	//# The $Bivector3D$ class is used to store a three-dimensional bivector having three floating-point components
+	//# named $x$, $y$, and $z$.
+	//#
+	//# The default constructor leaves the components of the bivector undefined.
+	//#
+	//# If points $p$ and $q$ are specified, then the bivector is initialized to the wedge product between homogeneous
+	//# extensions of $p$ and $q$ with <i>z</i> coordinates set to 1, giving a representation of the 2D line containing
+	//# both points. If the point $p$ and the direction $v$ are specified, then the line contains the point $p$ and runs
+	//# parallel to the direction $v$.
+	//
+	//# \operator	Bivector3D& operator +=(const Bivector3D& v);
+	//#				Adds the bivector $v$.
+	//
+	//# \operator	Bivector3D& operator -=(const Bivector3D& v);
+	//#				Subtracts the bivector $v$.
+	//
+	//# \operator	Bivector3D& operator *=(float s);
+	//#				Multiplies by the scalar $s$.
+	//
+	//# \operator	Bivector3D& operator /=(float s);
+	//#				Multiplies by the inverse of the scalar $s$.
+	//
+	//# \action		Bivector3D operator !(const Vector3D& v);
+	//#				Returns the complement of the vector $v$.
+	//
+	//# \action		Vector3D operator !(const Bivector3D& v);
+	//#				Returns the complement of the bivector $v$.
+	//
+	//# \action		Bivector3D operator -(const Bivector3D& v);
+	//#				Returns the negation of the bivector $v$.
+	//
+	//# \action		Bivector3D operator +(const Bivector3D& a, const Bivector3D& b);
+	//#				Returns the sum of the bivectors $a$ and $b$.
+	//
+	//# \action		Bivector3D operator -(const Bivector3D& a, const Bivector3D& b);
+	//#				Returns the difference of the bivectors $a$ and $b$.
+	//
+	//# \action		Bivector3D operator *(const Bivector3D& v, float s);
+	//#				Returns the product of the bivector $v$ and the scalar $s$.
+	//
+	//# \action		Bivector3D operator *(float s, const Bivector3D& L);
+	//#				Returns the product of the bivector $v$ and the scalar $s$.
+	//
+	//# \action		Bivector3D operator /(const Bivector3D& v, float s);
+	//#				Returns the product of the bivector $v$ and the inverse of the scalar $s$.
+	//
+	//# \action		bool operator ==(const Bivector3D& a, const Bivector3D& b);
+	//#				Returns a boolean value indicating whether the two bivectors $a$ and $b$ are equal.
+	//
+	//# \action		bool operator !=(const Bivector3D& a, const Bivector3D& b);
+	//#				Returns a boolean value indicating whether the two bivectors $a$ and $b$ are not equal.
+	//
+	//# \action		Bivector3D operator ^(const Vector3D& a, const Vector3D& b);
+	//#				Returns the wedge product of the vectors $a$ and $b$.
+	//
+	//# \action		Bivector3D operator ^(const Point2D& p, const Point2D& q);
+	//#				Returns the wedge product of the 2D points $p$ and $q$. The <i>z</i> coordinates of $p$ and $q$ are assumed to be 1.
+	//
+	//# \action		Bivector3D operator ^(const Point2D& p, const Vector2D& v);
+	//#				Returns the wedge product of the 2D point $p$ and the 2D vector $v$. The <i>z</i> coordinate of $p$ is assumed to be 1.
+	//
+	//# \action		Vector3D operator ^(const Bivector3D& a, const Bivector3D& b);
+	//#				Returns the antiwedge product of the bivectors $a$ and $b$.
+	//
+	//# \action		float operator ^(const Bivector3D& a, const Vector3D& b);
+	//#				Returns the antiwedge product of the bivector $a$ and the vector $b$.
+	//
+	//# \action		float operator ^(const Vector3D& a, const Bivector3D& b);
+	//#				Returns the antiwedge product of the vector $a$ and the bivector $b$.
+	//
+	//# \also	$@Vector3D@$
+	//# \also	$@Vector2D@$
+	//# \also	$@Point2D@$
+
+
+	//# \function	Bivector3D::Set		Sets all three components of a bivector.
+	//
+	//# \proto	Bivector3D& Set(float a, float b, float c);
+	//# \proto	Bivector3D& Set(const Point2D& p, const Point2D& q);
+	//# \proto	Bivector3D& Set(const Point2D& p, const Vector2D& v);
+	//
+	//# \param	a		The value of the <b>e</b><sub>23</sub> coordinate.
+	//# \param	b		The value of the <b>e</b><sub>31</sub> coordinate.
+	//# \param	c		The value of the <b>e</b><sub>12</sub> coordinate.
+	//# \param	p,q		Two 2D points that lie on the line.
+	//# \param	v		A 2D direction vector.
+	//
+	//# \desc
+	//# The $Set$ function replaces all three components of a bivector with new values.
+	//#
+	//# If points $p$ and $q$ are specified, then the bivector is initialized to the wedge product between homogeneous
+	//# extensions of $p$ and $q$ with <i>z</i> coordinates set to 1, giving a representation of the 2D line containing
+	//# both points. If the point $p$ and the direction $v$ are specified, then the line contains the point $p$ and runs
+	//# parallel to the direction $v$.
+	//#
+	//# The return value is a reference to the bivector object.
+
+
 	class Bivector3D : public Antivec3D<TypeBivector3D>
 	{
 		public:
@@ -50,9 +166,9 @@ namespace Terathon
 			template <typename type>
 			explicit Bivector3D(const Antivec3D<type>& v) : Antivec3D<TypeBivector3D>(float(v.x), float(v.y), float(v.z)) {}
 
-			Bivector3D(const Point2D& a, const Point2D& b)
+			Bivector3D(const Point2D& p, const Point2D& q)
 			{
-				xyz.Set(a.y - b.y, b.x - a.x, a.x * b.y - a.y * b.x);
+				xyz.Set(p.y - q.y, q.x - p.x, p.x * q.y - p.y * q.x);
 			}
 
 			Bivector3D(const Point2D& p, const Vector2D& v)
@@ -71,15 +187,15 @@ namespace Terathon
 				xyz.Set(a, b, c);
 			}
 
-			Bivector3D& Set(const Point2D& a, const Point2D& b)
+			Bivector3D& Set(const Point2D& p, const Point2D& q)
 			{
-				xyz.Set(a.y - b.y, b.x - a.x, a.x * b.y - a.y * b.x);
+				xyz.Set(p.y - q.y, q.x - p.x, p.x * q.y - p.y * q.x);
 				return (*this);
 			}
 
-			void Set(const Point2D& a, const Point2D& b) volatile
+			void Set(const Point2D& p, const Point2D& q) volatile
 			{
-				xyz.Set(a.y - b.y, b.x - a.x, a.x * b.y - a.y * b.x);
+				xyz.Set(p.y - q.y, q.x - p.x, p.x * q.y - p.y * q.x);
 			}
 
 			Bivector3D& Set(const Point2D& p, const Vector2D& v)
@@ -231,9 +347,9 @@ namespace Terathon
 		return (Bivector3D(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x));
 	}
 
-	inline Bivector3D operator ^(const Point2D& a, const Point2D& b)
+	inline Bivector3D operator ^(const Point2D& p, const Point2D& q)
 	{
-		return (Bivector3D(a.y - b.y, b.x - a.x, a.x * b.y - a.y * b.x));
+		return (Bivector3D(p.y - q.y, q.x - p.x, p.x * q.y - p.y * q.x));
 	}
 
 	inline Bivector3D operator ^(const Point2D& p, const Vector2D& v)
@@ -267,9 +383,9 @@ namespace Terathon
 		return (a ^ b);
 	}
 
-	inline Bivector3D Wedge(const Point2D& a, const Point2D& b)
+	inline Bivector3D Wedge(const Point2D& p, const Point2D& q)
 	{
-		return (a ^ b);
+		return (p ^ q);
 	}
 
 	inline Bivector3D Wedge(const Point2D& p, const Vector2D& v)
